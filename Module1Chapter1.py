fe4fc74537e2636be115c40725039bc142fa9658
edwinafrawley <edwinafrawley@gmail.com>
2021-04-10 17:28:05 +0100

    Version 1

---
+++ Module1Chapter1.py
@@ -0,0 +1,42 @@
+# Example 1, do not modify!
+print(8 / 2)
+
+# Example 2, do not modify!
+print(2**2)
+
+# Addition, no print output
+10+2
+(10+2)
+
+# Print the total revenue
+total = revenue_1 + revenue_2 + revenue_3
+print(total)
+
+# Print the average revenue
+average = (revenue_1+revenue_2+revenue_3)/(3)
+print(average)
+
+# Type of company_1
+print(type(company_1))
+
+# Type of year_1
+print(type(year_1))
+
+# Type of revenue_1
+print(type(revenue_1))
+
+# Test equality
+test_company = 'apple'
+print(company_1==test_company)
+
+# Update data types
+year_1_str = str(year_1)
+revenue_1_str = str(revenue_1)
+
+# Create a complete sentence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + year_1_str  + ' was $' + revenue_1_str + ' billion.'
+
+# Print sentence
+print(sentence)
+
+
